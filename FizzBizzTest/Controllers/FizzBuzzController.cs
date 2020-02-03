3d7a8305251c2205d2a4a0905f5a823e01e18d51
Floren <magrinyafloren@gmail.com>
2020-02-03 02:54:21 +0100

    txt write exception

---
+++ FizzBizzTest/Controllers/FizzBuzzController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace FizzBizzTest.Models
+{
+    public class FizzBuzzController : ApiController
+    {
+        const int MINNUM = 1;
+        const int MAXNUM = 100;
+        const string path = @"C:\Users\magri\Desktop\test\test.txt";
+
+        public IHttpActionResult GetFizzBuzz()
+        {
+            FizzBuzz fb = new FizzBuzz();
+            Random rnd = new Random();
+            int n = rnd.Next(MINNUM, MAXNUM);
+            List<string> fizzList = fb.GetFizzBuzz(n);
+            string s = "";
+
+            if (fizzList.Count < 1)
+            {
+                return NotFound();
+            }
+            s = string.Join(", ", fizzList);
+            
+            s = string.Concat(s + " " + DateTime.Now.ToString("M/d/yyyy hh:mm:ss"));
+
+            if (File.Exists(path))
+            {
+            using (StreamWriter sw = File.AppendText(path))
+            {
+                    try
+                    {
+                        sw.WriteLine(s);
+                    }
+                    catch
+                    {
+                        return NotFound();
+                    }
+            }
+            }
+
+            return Ok(s);
+        }
+    }
+}
---
+++ UnitTestProject/UnitTest1.cs
@@ -0,0 +1,19 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            FizzBizzTest.Models.FizzBuzz fizz = new FizzBizzTest.Models.FizzBuzz();
+            Random rnd = new Random();
+            int n = rnd.Next(1, 100);
+            fizz.GetFizzBuzz(n);
+
+        }
+    }
+}
